585a259211d0d20111020d3ba263213408c2bca2
Xia Dong <dongxiapp@gmail.com>
2019-04-16 19:07:04 +0000

    update the second store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id:1)
+puts @store1.name
+
+@store2 = Store.second
+puts @store2.name
+
+@store2.update(name: 'Coquitlam')
+puts @store2.name
