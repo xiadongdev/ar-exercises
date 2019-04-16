15838a09e551a2368ae3df555465a5676c612c08
Xia Dong <dongxiapp@gmail.com>
2019-04-16 22:21:00 +0000

    add validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+p = Employee.create(hourly_rate: 201)
+puts p.valid?
+puts p.errors.messages
+
+s = Store.create(name: "CA", annual_revenue: 33.3, mens_apparel: false, womens_apparel: false)
+puts s.valid?
+puts s.errors.details[:name]
+puts s.errors.details[:annual_revenue]
+puts s.errors.details[:mens_apparel]
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    errors.add(:mens_apparel, "must carry the men's or women's apparel") unless (mens_apparel || womens_apparel)
+    errors.add(:womens_apparel, "must carry the men's or women's apparel") unless (mens_apparel || womens_apparel)
+  end
 end
