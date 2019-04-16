c7d79e50455796d0caeb3aea274058f9431152b3
Xia Dong <dongxiapp@gmail.com>
2019-04-16 19:45:08 +0000

    calculate total/average sales and find stores make more than 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total Revenue: #{Store.sum(:annual_revenue)}"
+puts "Store Average Revenue: #{Store.sum(:annual_revenue)/Store.count}"
+puts "Number of stores with more than 1M sales: #{Store.where('annual_revenue >= 1000000').count} "
